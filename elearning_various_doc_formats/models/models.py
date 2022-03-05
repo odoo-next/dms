e60ec964e81dfc9a0e2eb1e707dc5918a949cdbc
dmontero <dmontero@quanam.com>
2022-03-05 15:42:08 -0300

    fix

--- elearning_various_doc_formats/models/models.py
+++ elearning_various_doc_formats/models/models.py
@@ -55,7 +55,7 @@ class SlideInherit(models.Model):
                     record.embed_code = '<iframe src="%s" class="o_wslides_iframe_viewer" allowFullScreen="true" height="%s" width="%s" frameborder="0"></iframe>' % (slide_url, 315, 420)
                 else:
                     base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-                    access_url = base_url +"/my/elearning/file/download/%s/%s" % (record.id,record.file_name)
+                    access_url = base_url +"/my/elearning/file/download/%s/%s" % (record.id,record.filename)
                     record.embed_code = '<iframe src="https://view.officeapps.live.com/op/embed.aspx?src=%s" class="o_wslides_iframe_viewer" allowFullScreen="true" height="%s" width="%s" frameborder="0"></iframe>' % (
                         access_url, 315, 420)
 
